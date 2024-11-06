3dfa64fdd7fd3ca47e1630d08f13ca9d6de1e822
8010488823 <alphagoku.28.06@gmail.com>
2024-11-06 23:51:32 +0530

    initial commit

---
+++ demo.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main	() {
+	fmt.Print("2+2")
+	fmt.Println("The time is" + "\n", time.Now())
+	fmt.Printf("My favorite number is" + "\n")
+	fmt.Println("My favorite number is", math.Sqrt(25))
+} 
